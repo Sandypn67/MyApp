995ad814ea9225bcaeca172329a5bccc52822cb1
sandypn67 <sandrine.miconnet@gmail.com>
2021-03-22 14:52:17 +0100

    TD2 sans trop erreur (mais la liste ne s'affiche pas)

--- app/src/main/java/com/example/esiea3a/presentation/api/PokeApi.kt
+++ app/src/main/java/com/example/esiea3a/presentation/api/PokeApi.kt
@@ -2,7 +2,7 @@ package com.example.esiea3a.presentation.api
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
+//import retrofit2.http.Query
 //@Query("200")
 
 interface PokeApi {
